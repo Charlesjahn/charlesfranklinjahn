becaf6f4bf9c8c9ec11d1f92d55fe22b10401751
Charles Franklin Jahn <charles.jahn@gmail.com>
2023-06-07 11:54:26 +0100

    Create scriptNav.js

---
+++ scriptNav.js
@@ -0,0 +1,24 @@
+var header = document.getElementById("header");
+var navigationHeader = document.getElementById("nagivation_header");
+var mainContainer = document.getElementById("mainContainer");
+var showSideBar = false;
+
+function sideBarOpenClose() {
+    showSideBar = !showSideBar;
+    if (showSideBar) {
+        navigationHeader.style.marginLeft = "0vw";
+        navigationHeader.style.animationName = "showSideBar";
+        mainContainer.style.filter = "blur(3px)"
+    } else {
+        navigationHeader.style.marginLeft = "-100vw";
+        navigationHeader.style.animationName = "";
+        
+        mainContainer.style.filter = "blur(0px)"
+    }
+}
+
+function sideBarClose() {
+    if (showSideBar) {
+        sideBarOpenClose()
+    }
+}
